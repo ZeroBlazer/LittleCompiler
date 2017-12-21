13bcb5edfe16b561ee04d91cb66ab4275542a5dc
Elías Puma <elpumitaelias@gmail.com>
2017-12-21 13:11:39 -0500

    token scanner and new types added

--- libs/compiler/src/lib.rs
+++ libs/compiler/src/lib.rs
@@ -1,5 +1,9 @@
+extern crate token_scanner;
+
 use std::fs::File;
 use std::io::prelude::*;
+
+use token_scanner::token_scanner;
 
 fn load_file(path: &str) -> Result<String, std::io::Error> {
     let mut file = File::open(path)?;
@@ -10,7 +14,9 @@ fn load_file(path: &str) -> Result<String, std::io::Error> {
 
 pub fn compile(path: &str) {
     if let Ok(input) = load_file(path) {
-        println!("{}", input);
+        // Steps to compile file
+        let tokens = token_scanner(&input);
+        println!("{:?}", tokens);
     } else {
         println!("Error while reading file!");
     }
