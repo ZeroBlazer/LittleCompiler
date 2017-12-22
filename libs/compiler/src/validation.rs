093a569c28a8502ebeecea0023432e2d241eb3c1
Elías Puma <elpumitaelias@gmail.com>
2017-12-21 20:12:37 -0500

    Minor changes

--- libs/compiler/src/validation.rs
+++ libs/compiler/src/validation.rs
@@ -4,6 +4,8 @@ use ansi_term::Colour::*;
 
 pub fn validate_and_prefix(tokens: VecDeque<(TokenType, String)>) -> Result<(), String> {
     let mut errors = String::new();
+
+    
     errors.push_str(format!("{} text", Yellow.bold().paint("Some")).as_str());
     
     Err(errors)
